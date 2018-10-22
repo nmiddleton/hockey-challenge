3938efa0625f4b69b532eadbbd81827357c5be2f
neil-middleton <neil.middleton@hotmail.com>
2018-10-22 18:03:00 +0100

    (new test) A function to scrape each divisions's fixtures

--- tests/mocha-spec/Scrape.spec.js
+++ tests/mocha-spec/Scrape.spec.js
@@ -7,15 +7,10 @@ const expect = require('chai').expect,
     expected_league_as_json = require('./expectations/expected_league_as_json.js'),
     expected_fixtures_as_json = require('./expectations/expected_fixtures_as_json.js');
 
-let sandbox;
+let sandbox,
+    expected_league_divisions = [ '3se', '4se', '5se', '6se', '7se', '8se', '9se', '10se' ];
 describe('Scraping..', function () {
     describe('the EML Tables', function () {
-        // it('stubbing works', function () {
-        //     request.get('something').then(function (result) {
-        //         expect(result).to.equal(eml_table_html());
-        //     });
-        // });
-
         beforeEach(function () {
             sandbox = sinon.sandbox.create();
             sandbox.stub(request, 'get').resolves(eml_table_html());
@@ -24,17 +19,37 @@ describe('Scraping..', function () {
         afterEach(function () {
             sandbox.restore();
         });
-        it('should parse the html into an object', function (done) {
+        it('should parse the EML Table html into an object', function (done) {
             Scrape().EMLTables().then(function (result) {
                 expect(result).to.deep.equal(expected_league_as_json());
             }).done(done);
         });
         it('should collect the divisions so that we can then query the fixtures for each division', function (done) {
-            let expected_league_divisions = [ '3se', '4se', '5se', '6se', '7se', '8se', '9se', '10se' ];
             Scrape().EMLTables().then(function (result) {
                 expect(Scrape().getLeagueDivisions(result)).to.deep.equal(expected_league_divisions);
             }).done(done);
         });
     });
+    describe('the EML Fixtures', function () {
+        beforeEach(function () {
+            sandbox = sinon.sandbox.create();
+            sandbox.stub(request, 'get').resolves(eml_fixtures_html());
+
+        });
+        afterEach(function () {
+            sandbox.restore();
+        });
+        // it('the stub works', function () {
+        //     request.get('something').then(function (result) {
+        //         expect(result).to.equal(eml_fixtures_html());
+        //     });
+        // });
+        it('should parse the EML Fixtures html into an object', function (done) {
+            Scrape().EMLFixtures(expected_league_divisions).then(function (result) {
+                expect(result).to.deep.equal(expected_fixtures_as_json());
+            }).done(done);
+        });
+    });
+
 });
 
