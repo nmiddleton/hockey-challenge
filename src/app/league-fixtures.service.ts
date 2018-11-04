3b0c2e0abd5a39ea416fa05ea42f53ec4b82f4d2
nmiddleton <ngmiddleton@hotmail.com>
2018-11-04 09:49:41 +0000

    (test passed) league fixture service

---
+++ src/app/league-fixtures.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {catchError} from "rxjs/operators";
+import {serviceHelpers} from "./service-helpers";
+import {environment} from "../environments/environment";
+
+const API_URL = environment.apiURL;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeagueFixturesService {
+
+  constructor(
+    private http: HttpClient) {
+  }
+
+  public getFixtureList(): Observable<any> {
+    return this.http.get<string>(API_URL + '/league_fixtures')
+      .pipe(
+        catchError(serviceHelpers.handleErrorAndContinue('get league_fixtures', []))
+      );
+  }
+
+  public getFixtureListFor(team: string): Observable<any> {
+    return this.http.get<string>(API_URL + '/league_fixtures/' + team)
+      .pipe(
+        catchError(serviceHelpers.handleErrorAndContinue('get league_fixtures/' + team, []))
+      );
+  }
+}
