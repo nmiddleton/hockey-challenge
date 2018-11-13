ee9aad1d98bb0dc90034020030f00f5dfe5c1d3a
nmiddleton <ngmiddleton@hotmail.com>
2018-11-13 21:55:45 +0000

    (debug) test without watch (so that it completes)

--- src/app/team-prediction/team-prediction.component.spec.ts
+++ src/app/team-prediction/team-prediction.component.spec.ts
@@ -68,12 +68,12 @@ describe('TeamPredictionComponent', () => {
     //   expect(expected_team_prediction.score_total).toEqual(3);
     //   expect(expected_team_prediction.home_or_away).toEqual('Home');
     // });
-    // it('getLeagueStrength', () => {
-    //   expect(component.getLeagueStrength(team_performance)).toEqual(3.0);
-    // });
-    // it('getDefensiveWeakness', () => {
-    //   expect(component.getDefensiveWeakness(team_performance)).toEqual(1.2);
-    // });
+    it('getLeagueStrength', () => {
+      expect(component.getLeagueStrength(team_performance)).toEqual(3.0);
+    });
+    it('getDefensiveWeakness', () => {
+      expect(component.getDefensiveWeakness(team_performance)).toEqual(1.2);
+    });
     it('getOffensiveStrength', () => {
       expect(component.getOffensiveStrength(oppo_team_performance)).toEqual(2.8);
     });
