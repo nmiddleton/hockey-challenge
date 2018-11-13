15c0eb72aac8c5003464426dfb2379da9156ffb4
nmiddleton <ngmiddleton@hotmail.com>
2018-11-13 15:17:32 +0000

    (test) use PhantomJS for karma testing on linux - fixing up polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -45,7 +45,12 @@
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following for the Reflect API. */
-// import 'core-js/es6/reflect';
+import 'core-js/es6/reflect';
+
+
+/** Evergreen browsers require these. **/
+import 'core-js/es7/reflect';
+import 'core-js/client/shim';
 
 /**
  * Web Animations `@angular/platform-browser/animations`
@@ -77,4 +82,12 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
+ */
+/**
+ * Date, currency, decimal and percent pipes.
+ * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
+ */
+import 'intl';
+/**
+ * Need to import at least one locale-data with intl.
  */
