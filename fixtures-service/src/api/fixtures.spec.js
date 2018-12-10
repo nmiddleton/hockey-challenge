4009f1ef26a4552bf28d857394b7ce3e05d22870
nmiddleton <ngmiddleton@hotmail.com>
2018-12-10 12:06:02 +0000

    (chore) amend tests with _id

--- fixtures-service/src/api/fixtures.spec.js
+++ fixtures-service/src/api/fixtures.spec.js
@@ -5,25 +5,29 @@ const server = require('../server/server')
 describe('Fixtures API', () => {
   let app = null
   let test_fixtures = [{
-    'away_team': 'Basildon 1',
-    'division': '3se',
-    'fixture_date': '22-Sep-18',
-    'home_team': 'Maldon 2'
+    away_team: 'Basildon 1',
+    division: '3se',
+    fixture_date: '22-Sep-18',
+    home_team: 'Maldon 2',
+    _id: '22-Sep-18:Basildon 1'
   }, {
-    'away_team': 'Maldon 2',
-    'division': '3se',
-    'fixture_date': '29-Sep-18',
-    'home_team': 'Old Loughts Academy'
+    away_team: 'Maldon 2',
+    division: '3se',
+    fixture_date: '29-Sep-18',
+    home_team: 'Old Loughts Academy',
+    _id: '29-Sep-18:Old Loughts Academy',
   }, {
-    'away_team': 'Maldon 2',
-    'division': '4se',
-    'fixture_date': '08-Dec-18',
-    'home_team': 'Basildon 1'
+    away_team: 'Maldon 2',
+    division: '4se',
+    fixture_date: '08-Dec-18',
+    home_team: 'Basildon 1',
+    _id: '08-Dec-18:Basildon 1'
   }, {
-    'away_team': 'Old Loughts Academy',
-    'division': '4se',
-    'fixture_date': '12-Jan-19',
-    'home_team': 'Maldon 2'
+    away_team: 'Old Loughts Academy',
+    division: '4se',
+    fixture_date: '12-Jan-19',
+    home_team: 'Maldon 2',
+    _id: '12-Jan-19:Maldon 2'
   }];
   // const mock_fixtures_source = require('../../../tests/mocha-spec/expectations/expected_fixtures_as_collection.js')
   const sourceAllFixtures = () => {
@@ -58,10 +62,11 @@ describe('Fixtures API', () => {
       .get('/fixtures')
       .expect((res) => {
         res.body.should.containEql({
-          'away_team': 'Maldon 2',
-          'division': '3se',
-          'fixture_date': '29-Sep-18',
-          'home_team': 'Old Loughts Academy'
+          _id: '29-Sep-18:Old Loughts Academy',
+          away_team: 'Maldon 2',
+          division: '3se',
+          fixture_date: '29-Sep-18',
+          home_team: 'Old Loughts Academy'
         })
       })
       .expect(200, done)
@@ -71,10 +76,11 @@ describe('Fixtures API', () => {
       .post('/fixtures')
       .expect((res) => {
         res.body.should.containEql({
-          'away_team': 'Maldon 2',
-          'division': '3se',
-          'fixture_date': '29-Sep-18',
-          'home_team': 'Old Loughts Academy'
+          _id: '29-Sep-18:Old Loughts Academy',
+          away_team: 'Maldon 2',
+          division: '3se',
+          fixture_date: '29-Sep-18',
+          home_team: 'Old Loughts Academy'
         })
       })
       .expect(200, done)
