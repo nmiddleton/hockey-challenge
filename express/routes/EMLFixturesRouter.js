64923f55416d2a5950503504a8ce6799b11e2689
nmiddleton <ngmiddleton@hotmail.com>
2018-12-11 19:37:52 +0000

    (deprecate) micro services API now replace the monolithic express api

--- express/routes/EMLFixturesRouter.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-  router = express.Router(),
-  getEMLFixturesPageForScrape = require('../lib/getEMLFixturesPageForScrape'),
-  getEMLTablePageForScrape = require('../lib/getEMLTablePageForScrape');
-
-//  when http://localhost:3333/EMLFixtures/3se    is received
-router.get('/:id', getEMLFixturesPageForScrape, function(req, res){});
-
-module.exports = router;
